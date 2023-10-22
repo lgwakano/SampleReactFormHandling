51273be26312d75891409d54f2f293eec392b026
Luiz <lgwakano@gmail.com>
2023-10-22 02:40:49 -0600

    add form, onchange and onsubmit handle functions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import BookList from './components/BookList';
 import BookCreate from './components/BookCreate';
 
 function App(){
-    const [books, setBooks] = useState({});
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        console.log('create book', title);
+        setBooks([...books,{title: title}]);
+    };
+
+    const editBook = () => {
+        
+    };
+
+    const deleteBook = () => {
+        
+    };
 
     return <div>
         <BookList/>
-        <BookCreate/>
+        <BookCreate onCreateBook={createBook}/>
     </div>;
 }
 
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,25 @@
-function BookCreate(){
-    return <div>BookCreate</div>;
+import { useState } from "react";
+
+function BookCreate({onCreateBook}){
+    const [title, setTitle] = useState('');    
+    
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreateBook(title);
+        setTitle('');
+    }
+
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input onChange={handleChange} value={title}/>
+            <button>Create!</button>
+        </form>
+    </div>;
 }
 
 export default BookCreate;

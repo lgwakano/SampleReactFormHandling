5480cc147a7999701c0dd6c075ed7c2190b87083
Luiz <lgwakano@gmail.com>
2023-11-03 00:33:51 -0600

    added styling and format

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import { useState } from 'react';
 import BookList from './components/BookList';
 import BookCreate from './components/BookCreate';
 
-function App(){
+function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
         console.log('create book', title);
-        setBooks([...books,{title: title}]);
+        setBooks([...books, {
+            id: Math.round(Math.random() * 9999),
+            title
+        }]);
     };
 
     const editBook = () => {
-        
+
     };
 
     const deleteBook = () => {
-        
+
     };
 
-    return <div>
-        <BookList/>
-        <BookCreate onCreateBook={createBook}/>
+    return <div className='app'>
+        <BookList books={books} />
+        <BookCreate onCreateBook={createBook} />
     </div>;
 }
 
